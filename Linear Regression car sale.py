ca5e4091ef056d4ae1857e107a8831db11c65e98
goutham-hariharan <102451859+goutham-hariharan@users.noreply.github.com>
2022-07-30 00:54:14 -0700

    Add files via upload

---
+++ Linear Regression car sale.py
@@ -0,0 +1,402 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import seaborn as sns
+sns.set()
+
+
+# In[2]:
+
+
+raw_data = pd.read_csv("car sale.csv")
+raw_data
+
+
+# In[3]:
+
+
+raw_data.describe(include = 'all')
+
+
+# In[4]:
+
+
+data = raw_data.drop(['Model'],axis=1)
+data.describe(include = 'all')
+
+
+# In[5]:
+
+
+data.isnull().sum()
+
+
+# In[6]:
+
+
+data_no_mv = data.dropna(axis = 0)
+
+
+# In[7]:
+
+
+data_no_mv.describe(include='all')
+
+
+# In[8]:
+
+
+sns.distplot(data_no_mv['Price'])
+
+
+# In[9]:
+
+
+q = data_no_mv['Price'].quantile(0.99)
+data_1 = data_no_mv[data_no_mv['Price']<q]
+data_1.describe(include='all')
+
+
+# In[10]:
+
+
+sns.distplot(data_1['Price'])
+
+
+# In[11]:
+
+
+sns.distplot(data_1['Mileage'])
+
+
+# In[12]:
+
+
+q = data_1['Mileage'].quantile(0.99)
+data_2 = data_1[data_1['Mileage']<q]
+sns.distplot(data_2['Mileage'])
+
+
+# In[13]:
+
+
+sns.distplot(data_2['EngineV'])
+
+
+# In[14]:
+
+
+data_3 = data_2[data_2['EngineV']<6.5]
+sns.distplot(data_3['EngineV'])
+
+
+# In[15]:
+
+
+sns.distplot(data_3['Year'])
+
+
+# In[16]:
+
+
+q = data_3['Year'].quantile(0.01)
+data_4 = data_3[data_3['Year']>q]
+sns.distplot(data_4['Year'])
+
+
+# In[17]:
+
+
+data_cleaned = data_4.reset_index(drop=True)
+data_cleaned.describe(include='all')
+
+
+# In[18]:
+
+
+f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True, figsize =(15,3)) #sharey -> share 'Price' as y
+ax1.scatter(data_cleaned['Year'],data_cleaned['Price'])
+ax1.set_title('Price and Year')
+ax2.scatter(data_cleaned['EngineV'],data_cleaned['Price'])
+ax2.set_title('Price and EngineV')
+ax3.scatter(data_cleaned['Mileage'],data_cleaned['Price'])
+ax3.set_title('Price and Mileage')
+
+
+plt.show()
+
+
+# In[19]:
+
+
+log_price = np.log(data_cleaned['Price'])
+
+data_cleaned['log_price'] = log_price
+data_cleaned
+
+
+# In[20]:
+
+
+f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True, figsize =(15,3))
+ax1.scatter(data_cleaned['Year'],data_cleaned['log_price'])
+ax1.set_title('Log Price and Year')
+ax2.scatter(data_cleaned['EngineV'],data_cleaned['log_price'])
+ax2.set_title('Log Price and EngineV')
+ax3.scatter(data_cleaned['Mileage'],data_cleaned['log_price'])
+ax3.set_title('Log Price and Mileage')
+
+
+plt.show()
+
+
+# In[21]:
+
+
+data_cleaned = data_cleaned.drop(['Price'],axis=1)
+
+
+# In[22]:
+
+
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+variables = data_cleaned[['Mileage','Year','EngineV']]
+
+vif = pd.DataFrame()
+
+vif["VIF"] = [variance_inflation_factor(variables.values, i) for i in range(variables.shape[1])]
+
+vif["Features"] = variables.columns
+
+
+# In[23]:
+
+
+vif
+
+
+# In[24]:
+
+
+data_no_multicollinearity = data_cleaned.drop(['Year'],axis=1)
+
+
+# In[25]:
+
+
+data_with_dummies = pd.get_dummies(data_no_multicollinearity, drop_first=True)
+
+
+# In[26]:
+
+
+data_with_dummies.head()
+
+
+# In[27]:
+
+
+data_with_dummies.columns.values
+
+
+# In[28]:
+
+
+cols = ['log_price', 'Mileage', 'EngineV', 'Brand_BMW',
+       'Brand_Mercedes-Benz', 'Brand_Mitsubishi', 'Brand_Renault',
+       'Brand_Toyota', 'Brand_Volkswagen', 'Body_hatch', 'Body_other',
+       'Body_sedan', 'Body_vagon', 'Body_van', 'Engine Type_Gas',
+       'Engine Type_Other', 'Engine Type_Petrol', 'Registration_yes']
+
+data_preprocessed = data_with_dummies[cols]
+data_preprocessed.head()
+
+
+# In[29]:
+
+
+targets = data_preprocessed['log_price']
+
+inputs = data_preprocessed.drop(['log_price'],axis=1)
+
+
+# In[30]:
+
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+
+scaler.fit(inputs)
+
+
+# In[31]:
+
+
+inputs_scaled = scaler.transform(inputs)
+
+
+# In[32]:
+
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(inputs_scaled, targets, test_size=0.2, random_state=365)
+
+
+# In[33]:
+
+
+reg = LinearRegression()
+
+reg.fit(x_train,y_train)
+
+
+# In[34]:
+
+
+y_hat = reg.predict(x_train)
+
+
+# In[35]:
+
+
+plt.scatter(y_train, y_hat)
+
+plt.xlabel('Targets (y_train)',size=18)
+plt.ylabel('Predictions (y_hat)',size=18)
+
+plt.xlim(6,13)
+plt.ylim(6,13)
+plt.show()
+
+
+# In[36]:
+
+
+sns.distplot(y_train - y_hat)
+
+plt.title("Residuals PDF", size=18)
+
+
+# In[37]:
+
+
+reg.score(x_train,y_train)
+
+
+# In[38]:
+
+
+reg.intercept_
+
+
+# In[39]:
+
+
+reg.coef_
+
+
+# In[40]:
+
+
+reg_summary = pd.DataFrame(inputs.columns.values, columns=['Features'])
+reg_summary['Weights'] = reg.coef_
+reg_summary
+
+
+# In[41]:
+
+
+data_cleaned['Brand'].unique()
+
+
+# In[42]:
+
+
+y_hat_test = reg.predict(x_test)
+
+
+# In[43]:
+
+
+plt.scatter(y_test, y_hat_test, alpha=0.2)
+plt.xlabel('Targets (y_test)',size=18)
+plt.ylabel('Predictions (y_hat_test)',size=18)
+plt.xlim(6,13)
+plt.ylim(6,13)
+plt.show()
+
+
+# In[44]:
+
+
+df_pf = pd.DataFrame(np.exp(y_hat_test), columns=['Prediction'])
+df_pf.head()
+
+
+# In[45]:
+
+
+df_pf['Target'] = np.exp(y_test)
+df_pf
+
+
+# In[46]:
+
+
+y_test = y_test.reset_index(drop=True)
+
+y_test.head()
+
+
+# In[47]:
+
+
+df_pf['Target'] = np.exp(y_test)
+df_pf
+
+
+# In[48]:
+
+
+df_pf['Residual'] = df_pf['Target'] - df_pf['Prediction']
+
+
+# In[49]:
+
+
+df_pf['Difference%'] = np.absolute(df_pf['Residual']/df_pf['Target']*100)
+df_pf
+
+
+# In[50]:
+
+
+df_pf.describe()
+
+
+# In[51]:
+
+
+pd.options.display.max_rows = 999
+
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+
+df_pf.sort_values(by=['Difference%'])
+
+
+# In[ ]:
+
+
+
+
